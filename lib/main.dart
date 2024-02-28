c49667d5ab4e12b53fe282d9ea38de90b4a0490c
rajascreamo <rajascreamo04@gmail.com>
2024-02-28 20:27:13 +0800

    set theme font family

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Landingpage(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Montserrat'),
     );
   }
 }
